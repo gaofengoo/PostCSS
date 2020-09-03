9c59c2fba9934c675636f72ce1f0df309c080637
HGF <15879831437@163.com>
2020-09-03 13:55:19 +0800

    更新示例

--- gulpfile.js
+++ gulpfile.js
@@ -3,45 +3,50 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
-var cssnano = require('cssnano');
 var stylelint = require('stylelint');
 var reporter = require('postcss-reporter');
-var cssvariables = require('postcss-css-variables');
+var sass = require('gulp-sass');
 
-gulp.task('autoprefixer', function () {
-	return gulp.src('src/*.css')
-	.pipe(postcss([autoprefixer, cssnano, cssvariables(/*options*/)]))
-	.pipe(gulp.dest('dest/'));
+gulp.task('autoprefixer', ['lint-styles'], function() {
+    return gulp.src('src/*.css')
+		.pipe(postcss([ autoprefixer ]))
+		.pipe(gulp.dest('dest/'));
 });
 
-gulp.task('rename', ['lint-styles'], function () {
+gulp.task('rename', ['autoprefixer'], function () {
 	return gulp.src('dest/*.css')
-	.pipe(postcss([ cssnano ]))
-	.pipe(rename('style.min.css'))
-	.pipe(sourcemaps.init())
-	.pipe(sourcemaps.write('maps/'))
-	.pipe(gulp.dest('dest/'));
+		.pipe(rename('style.min.css'))
+		.pipe(sourcemaps.init())
+		.pipe(sourcemaps.write('maps/'))
+		.pipe(gulp.dest("dest/"));
 });
 
-gulp.task("lint-styles", ['autoprefixer'], function() {
-	return gulp.src("src/*.css")
-	.pipe(postcss([ stylelint({ 
-			"rules": {
-				"color-no-invalid-hex": 2,
-				"declaration-colon-space-before": [2, "never"],
-				"indentation": [2, 2],
-				"number-leading-zero": [2, "always"]
-			}
-		}),
-		reporter({
-			clearMessages: true,
-		})
-	]))
+gulp.task("lint-styles", ['sass'], function() {
+    return gulp.src("src/*.css")
+    .pipe(postcss([ stylelint({ 
+        "rules": {
+          "color-no-invalid-hex": 2,
+          "declaration-colon-space-before": [2, "never"],
+          "indentation": [2, 2],
+          "number-leading-zero": [2, "always"]
+        }
+      }),
+      reporter({
+        clearMessages: true,
+      })
+    ]))
 });
 
-gulp.task('default', ['lint-styles', 'autoprefixer', 'rename']);
+gulp.task('sass', function () {
+  gulp.src('src/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('src/'));
+});
 
-var watcher = gulp.watch('src/*.css', ['default']);
-watcher.on('change', function (event) {
-	console.log('File' + event.path + ' was ' + event.type + ', running task...');
+
+gulp.task('default', ['sass', 'lint-styles', 'autoprefixer', 'rename']);
+
+var watcher = gulp.watch('src/*.scss', ['default']);
+watcher.on('change', function(event) {
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 });
